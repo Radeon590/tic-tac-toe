6816937fab8c21eb2a989aadd7d5faff40a46908
Rodion <59920979+Radeon590@users.noreply.github.com>
2024-05-04 15:36:43 +0300

    Storing a history of moves

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
 
-export default function Board(){
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}){
 
   function handleClick(i){
     if (squares[i] || calculateWinner(squares)){
@@ -20,8 +18,7 @@ export default function Board(){
       nextSquares[i] = "O";
 
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -52,6 +49,28 @@ export default function Board(){
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+  )
+}
+
+export default function Game(){
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const currentSquares = history[history.length - 1]
+
+  function handlePlay(nextSquares){
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{'TODO'}</ol>
+      </div>
+    </div>
   )
 }
 
